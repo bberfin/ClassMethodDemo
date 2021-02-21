2fc22d84497ab39ef476b46c38ee4dc2c122b889
BERFİN AÇIKGÖZ <berfinackgz@gmail.com>
2021-02-21 22:49:29 +0300

    Proje dosyası ekle.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("---------------Registration completed successfully---------------");
+            Console.WriteLine("Name/Surname: " + customer.Name + " " + customer.Surname);
+            Console.WriteLine("Age: " + customer.Age);
+            Console.WriteLine("Id: " + customer.Id);
+            Console.WriteLine("Explanation: " + customer.Explanation);
+            Console.WriteLine("-----------------------------------------------------------------\n");
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " has been deleted from the system.");
+        }
+
+        public void ListCustomer(Customer[] customers)
+        {
+            Console.WriteLine("Customer List:");
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id + " / " + customer.Name + " " + customer.Surname + " / " + customer.Age + " / " + customer.Explanation);
+
+            }
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Berfin";
+            customer1.Surname = "Acikgoz";
+            customer1.Age = 21;
+            customer1.Id = 12345;
+            customer1.Explanation = "credit";
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Tuba";
+            customer2.Surname = "Tasdemir";
+            customer2.Age = 30;
+            customer2.Id = 78932;
+            customer2.Explanation = "donation";
+
+            Customer customer3 = new Customer();
+            customer3.Name = "Harun";
+            customer3.Surname = "Ust";
+            customer3.Age = 20;
+            customer3.Id = 78932;
+            customer3.Explanation = "green card";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.AddCustomer(customer1);
+            customerManager.AddCustomer(customer2);
+            customerManager.AddCustomer(customer3);
+
+            //customerManager.DeleteCustomer(customer2);
+            customerManager.ListCustomer(customers);
+        }
+    }
+}
